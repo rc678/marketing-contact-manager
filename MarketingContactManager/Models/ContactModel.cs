a9e8fc254140d78c161e3fefa6c849902d4cf161
roshe <roshe@DESKTOP-9B7PIME>
2024-08-18 13:43:36 -0400

    Add project files.

---
+++ MarketingContactManager/Models/ContactModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MarketingContactManager.Models
+{
+    public record ContactModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set;  }
+
+
+    }
+}
